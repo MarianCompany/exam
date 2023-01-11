03fc1c2907b1424fc7d81c3588521cf616be73bf
mark <m.samojlenko@autodrive-agency.ru>
2023-01-11 15:43:22 +0300

    added search, filter and sort

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -5,7 +5,23 @@ export default {
   name: "MainView",
   data: () => {
     return {
-
+      searchQuery: null,
+      sortBy: '',
+      filterBy: '',
+      filterAlias: {
+        'easy': {
+          min: 0,
+          max: 3.5,
+        },
+        'medium': {
+          min: 3.6,
+          max: 7.5,
+        },
+        'hard': {
+          min: 7.6,
+          max: 10,
+        },
+      }
     }
   },
 
@@ -14,6 +30,35 @@ export default {
       fetchPrograms: 'fetchPrograms',
       fetchReviews: 'fetchReviews',
     }),
+
+    changePriceSort() {
+      if (this.sortBy === 'priceToDown') {
+        this.sortBy = 'priceToTop';
+      } else if (this.sortBy === 'priceToTop') {
+        this.sortBy = '';
+      } else {
+        this.sortBy = 'priceToDown';
+      }
+    },
+
+    changePopularitySort() {
+      if (this.sortBy === 'popularityToDown') {
+        this.sortBy = 'popularityToTop';
+      } else if (this.sortBy === 'popularityToTop') {
+        this.sortBy = '';
+      } else {
+        this.sortBy = 'popularityToDown';
+      }
+    },
+
+    changeProgramsFilter(query) {
+      if (query === this.filterBy) {
+        this.filterBy = '';
+        return 0;
+      }
+
+      this.filterBy = query;
+    },
   },
 
   computed: {
@@ -23,6 +68,37 @@ export default {
       areReviewsLoaded: 'getAreReviewsLoaded',
       reviews: 'getReviews',
     }),
+
+    filteredPrograms() {
+      let result = this.programs;
+
+      if (this.searchQuery) {
+        result = result.filter(function(el) {
+          return el.title.startsWith(this.searchQuery) || el.title.endsWith(this.searchQuery) || el.title.includes(this.searchQuery);
+        }.bind(this));
+      }
+
+      if (this.filterBy) {
+        result = result.filter(function(el) {
+          return el.difficulty >= this.filterAlias[this.filterBy].min
+              && el.difficulty <= this.filterAlias[this.filterBy].max;
+        }.bind(this));
+      }
+
+      if (this.sortBy) {
+        if (this.sortBy === 'priceToDown') {
+          result = result.sort((a, b) => b.price - a.price);
+        } else if (this.sortBy === 'priceToTop') {
+          result = result.sort((a, b) => a.price - b.price);
+        } else if (this.sortBy === 'popularityToDown') {
+          result = result.sort((a, b) => b.popularity - a.popularity);
+        } else if (this.sortBy === 'popularityToTop') {
+          result = result.sort((a, b) => a.popularity - b.popularity);
+        }
+      }
+
+      return result;
+    },
   },
 
   created() {
@@ -56,7 +132,7 @@ export default {
       </div>
       <div v-if="areProgramsLoaded" class="programs__wrap">
         <div class="search">
-          <input type="text" class="search__input" inputmode="text" placeholder="Поиск">
+          <input type="text" class="search__input" inputmode="text" placeholder="Поиск" v-model="searchQuery">
         </div>
         <div class="controls">
           <div class="controls__block">
@@ -64,11 +140,40 @@ export default {
               Сортировка:
             </p>
             <div class="controls__wrap">
-              <button class="controls__btn controls__btn_sort">
-                По цене <img src="/img/chevron.svg" class="controls__btn-chevron">
+              <button
+                  @click.prevent="changePriceSort"
+                  class="controls__btn controls__btn_sort"
+                  :class="{
+                    'controls__btn_active': sortBy.startsWith('price'),
+                  }"
+              >
+                По цене
+                <img
+                    src="/img/chevron.svg"
+                    class="controls__btn-chevron"
+                    :class="{
+                      'controls__btn-chevron_up': sortBy === 'priceToDown',
+                      'controls__btn-chevron_active': sortBy && sortBy.startsWith('price'),
+                    }"
+                >
               </button>
-              <button class="controls__btn  controls__btn_sort">
-                По популярности <img src="/img/chevron.svg" class="controls__btn-chevron">
+              <button
+                  class="controls__btn controls__btn_sort"
+                  :class="{
+                    'controls__btn_active': sortBy.startsWith('popularity'),
+                  }"
+                  @click.prevent="changePopularitySort"
+              >
+                По популярности
+                <img
+                    v-show="sortBy && sortBy.startsWith('popularity')"
+                    src="/img/chevron.svg"
+                    class="controls__btn-chevron"
+                    :class="{
+                      'controls__btn-chevron_up': sortBy === 'popularityToDown',
+                      'controls__btn-chevron_active': sortBy && sortBy.startsWith('popularity'),
+                    }"
+                >
               </button>
             </div>
           </div>
@@ -77,21 +182,39 @@ export default {
               Фильтрация:
             </p>
             <div class="controls__wrap">
-              <button class="controls__btn">
+              <button
+                  @click="changeProgramsFilter('easy')"
+                  class="controls__btn"
+                  :class="{
+                    'controls__btn_active': filterBy === 'easy',
+                  }"
+              >
                 Легко
               </button>
-              <button class="controls__btn">
+              <button
+                  @click="changeProgramsFilter('medium')"
+                  class="controls__btn"
+                  :class="{
+                    'controls__btn_active': filterBy === 'medium',
+                  }"
+              >
                 Средне
               </button>
-              <button class="controls__btn">
+              <button
+                  @click="changeProgramsFilter('hard')"
+                  class="controls__btn"
+                  :class="{
+                    'controls__btn_active': filterBy === 'hard',
+                  }"
+              >
                 Сложно
               </button>
             </div>
           </div>
         </div>
-        <div class="programs__list">
+        <div class="programs__list" v-if="filteredPrograms.length">
           <router-link
-              v-for="item in programs"
+              v-for="item in filteredPrograms"
               class="programs__item"
               :key="item.title"
               :to="`/programs/${item.id}`"
@@ -118,6 +241,9 @@ export default {
             </div>
           </router-link>
         </div>
+        <p class="programs__text programs__msg" v-else>
+           К сожалению, таких программ нет :(
+        </p>
       </div>
     </div>
     <div class="review">
