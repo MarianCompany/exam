a9d89f2a7cbfacf8d66d76fb6e72ea569637416b
mark <m.samojlenko@autodrive-agency.ru>
2023-01-11 14:14:26 +0300

    added footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,11 @@
 <template>
   <footer class="footer">
     <div class="container">
-
+      <div class="footer__wrap">
+        <router-link to="/" class="nav__title footer__title">
+          Fitness4You
+        </router-link>
+      </div>
     </div>
   </footer>
 </template>
